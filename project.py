885f0989d1f79c556f8201377712a0e5488fd8ff
EbrahimKiani <ebimp4@gmail.com>
2021-11-28 21:55:52 +0330

    creat project file and make stack

---
+++ project.py
@@ -0,0 +1,26 @@
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)				
+
+    def pop(self):
+        return self.items.pop()
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        
+    def get_stack(self):
+        return self.items
+
+mystack = Stack()
+mystack.push('a')
+mystack.push('b')
+mystack.push('c')
+print(mystack.peek())
+print(mystack.peek())
