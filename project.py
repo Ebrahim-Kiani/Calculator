5cffbb1ebf95527ad1f1142f92050e5140300bc9
EbrahimKiani <ebimp4@gmail.com>
2021-12-08 23:25:50 +0330

    Error funcion has been created

--- project.py
+++ project.py
@@ -17,6 +17,28 @@ class Stack():
         
     def get_stack(self):
         return self.items
+def Error(userin):
+    Errorstack=Stack()
+    for item in userin:
+        if item ==')':
+            Errorstack.push(item)
+    for item in userin:
+        if item=='(':
+            if not Errorstack.is_empty():
+                Errorstack.pop()
+            else:
+                return False
+    if not Errorstack.is_empty():
+        return False
+    for item in range(0 ,len(userin)):
+        if userin[item] =='*' or userin[item]=='+' or userin[item] =='-' or userin[item] =='/' or userin[item] =='^':
+            try:
+                if  (not userin[item+1].isdigit()) and userin[item+1]!='(':
+                    return False
+            except:
+                return False
+    return True            
+
 
 def prioritize(a , b):
     if (a=='/' or a=='*') and (b=='/' or b=='*'):
@@ -89,14 +111,6 @@ def separate(userin):
             pass
 
         try:
-            #if list_userin[item].isdigit() and list_userin[item+1]==')' and list_userin[item-1]=='-'and list_userin[item-2]=='(':
-#string=''
-            #    string +=list_userin[item-1]
-            #    string +=list_userin[item]
-            #    list_userin.pop()
-            #    list_userin.pop()
-#list_userin.pop(item-1)
-            #    list_userin.pop(item-1)
             if list_userin[item].isdigit():
                 string=''
                 while list_userin[item]=='.'or list_userin[item].isdigit():
@@ -152,39 +166,11 @@ def calculator(string ,  negetivs):
             return answer
         else:
             return answer*-1
-userin='(1+3)*2^2'
-a,b=separate( userin+',')
 
-f=infix_to_postfix(a)
+userin='4^2'
+a,b=separate( userin+',')
 print(a)
-print(b)
-print(f)
+print(Error(userin))
+f=infix_to_postfix(a)
 print(calculator(f,b))
 
-#--------------------------------------------------------------------------------------------------
-#  Function			Description
-#--------------------------------------------------------------------------------------------------
-#  all()            Returns True if all the Boolean values in the list are True, else returns False
-#  any()			Returns True if any of the Boolean values in the list is True
-#  append()         Adds an item to the end of the list
-#  len()			Returns the numbers of items in a list
-#  insert()         Inserts an item at a given position
-#  pop([])          Removes the item at the given position in the list
-#  clear()          Removes all items from the list.           
-#  reverse()        Reverses the elements of the list
-#  sort()			Sorts the elements of the list.
-#  min()            Returns the smallest item in the list.
-#  max()            Returns the largest item in the list.
-#--------------------------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------------------
-#   name       			description
-#-------------------------------------------------------------------------------------------
-#   len()               Returns the number of characters in a string
-#   capitalize()        Converts the first character of a string to capital letter
-#   count()             Returns the number of occurrences of a substring in the given string
-#   lower()             Converts a string into lower case
-#   upper()	            Converts a string into upper case
-#   isspace()           Returns True if all characters in the string are whitespaces
-#   replace()           Replaces all or part of a string with another string
-#   isdigit()           Returns True if the string consists of digits only
-#-------------------------------------------------------------------------------------------
